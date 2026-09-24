87867a19ad6ca7045b7df4c281328b8176745b19
Vinicius Sylva <viniciussilva7494@gmail.com>
2026-09-24 06:22:42 -0300

    feat(webhook): update webhook file

--- src/api/v1/endpoints/webhooks.py
+++ src/api/v1/endpoints/webhooks.py
@@ -1,12 +1,24 @@
-from fastapi import APIRouter, status 
+import logging
+from fastapi import APIRouter, status, Depends
 from src.schemas.webhook_payload import IncidentWebhook
+from src.core.security import verify_api_key
+
+# Configuração do logger padrão da aplicação
+logger = logging.getLogger("uvicorn.error")
 
 router = APIRouter()
 
-@router.post("/alert", status_code=status.HTTP_202_ACCEPTED)
+
+@router.post(
+    "/alert",
+    status_code=status.HTTP_202_ACCEPTED,
+    dependencies=[Depends(verify_api_key)]
+)
 async def receive_alert(payload: IncidentWebhook):
-    print(f"[INCIDENTE DETECTADO] Serviço: {payload.service_name}")
-    print(f"Status: {payload.status} | Erro: {payload.error_message}")
+    logger.info(
+        f"[INCIDENTE DETECTADO] Serviço: {payload.service_name} | "
+        f"Status: {payload.status} | Erro: {payload.error_message}"
+    )
 
     return {
         "status": "received",
